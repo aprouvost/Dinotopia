5da2f66789d6862b18f3a1167439fafe28ef07d8
Jamesdu29 <DspreadT-H@gmx.com>
2019-03-16 18:32:44 +0100

    Correction PanStats

--- panStats.java
+++ panStats.java
@@ -1,50 +1,47 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-					
-public class panStats extends JPanel implements ActionListener {
-	
-	JLabel compteurTour;
-	JLabel propCarni;
-	JLabel propHerbi;
-	JLabel prctgDino;
-	JLabel prctgVegetaux;
-	
-	public panStats() { //CONSTRUCTEUR
-		
+
+public class PanStats extends JPanel implements ActionListener {
+
+	private JLabel compteurTour;
+	private JLabel propCarni;
+	private JLabel propHerbi;
+	private JLabel prctgDino;
+	private JLabel prctgVegetaux;
+
+	public PanStats() { //CONSTRUCTEUR
+
 		this.setBackground(new Color(0,0,0,0));
 		this.setBounds(410, 250, 780-410, 480-250);
 		this.setLayout(new GridLayout());
-		
+
 		//JLabels
-		this.compteurTour = new JLabel("Tour n°: ");
+		this.compteurTour = new JLabel("Tour nï¿½: ");
 		this.propCarni = new JLabel("Proportion de carnivores :");
 		this.propHerbi = new JLabel("Proportion d'herbivores :");
 		this.prctgDino = new JLabel("Pourcentage de dinosaures :");
-		this.prctgVegetaux = new JLabel("Pourcentage de végétaux :");
-		
-		
-		
-	}
-	
-	public void setCompteur(String mess) {
-		
-	}
-	public void setPropCarni(String mess) {
-		
-	}
-	public void setPropHerbi(String mess) {
-		
-	}
-	public void setPrctgDino(String mess) {
-		
-	}
-	public void setPrctgVege(String mess) {
-		this.prctgVegetaux.set
+		this.prctgVegetaux = new JLabel("Pourcentage de vï¿½gï¿½taux :");
+
 	}
 
-	
-	
+	public void setCompteur(String mess) {
+
+	}
+	public void setPropCarni(String mess) {
+
+	}
+	public void setPropHerbi(String mess) {
+
+	}
+	public void setPrctgDino(String mess) {
+
+	}
+	public void setPrctgVege(String mess) {
+	}
+
+
+
 	public void actionPerformed(ActionEvent arg0) {
 	}
 
